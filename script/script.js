1983848f0b3e0fadff5dca3259fb3e0d4407c779
naiden <na4ko359@gmail.com>
2022-01-24 16:38:19 +0200

    features section completed & responsive

--- script/script.js
+++ script/script.js
@@ -7,3 +7,37 @@ document.getElementById("hamburger").addEventListener("click", () => {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
     }
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides (n) {
+    let slides = document.getElementsByClassName("slide");
+    let buttons = document.getElementsByClassName("button");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].className = buttons[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "flex";
+    buttons[slideIndex-1].className += " active";
+}
