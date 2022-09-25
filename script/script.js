b02c1e49ebcab47658dc8448fbe8d736b6055266
naiiiden <na4ko359@gmail.com>
2022-09-25 22:27:53 +0300

    mobile menu button aria-label changes value on open/close

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 document.getElementById("hamburger").addEventListener("click", () => {
     document.getElementById("nav-ul").classList.toggle("show");
-    
     if (document.getElementById("menu-icon").getAttribute("src") === "images/icon-hamburger.svg") {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-close.svg");
+        document.querySelector("#hamburger").ariaLabel = "Close menu";
     } else {
         document.getElementById("menu-icon").setAttribute("src", "images/icon-hamburger.svg");
+        document.querySelector("#hamburger").ariaLabel = "Open menu";
     }
 });
 
